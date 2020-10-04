6f7ebc2f5eddd0932e3146ea47b2beba6153a756
2ujin <suddenlydisappear200@gmail.com>
2020-10-04 13:48:09 +0900

    Add bodyParser and cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+
 const app = express()
 const port = 4000
 
+
+/**
+ *  2020.10.04 이유진
+ *  REST 방식의 AP서버들은 웹 페이지의 본문 내용을 분석하려고 할 때 bodyParser와 cors 패키지를 use메서드를 사용해야한다.
+ */
 app
+    .use(bodyParser.urlencoded({extended: true})) 
+    .use(cors())
     .get('/', (req, res) => res.json({ message: 'hello world' }))
     .get('/hello/:skip/:limit', (req, res) => {
         console.log(req.params)
