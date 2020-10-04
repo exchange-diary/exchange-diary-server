9df1dd4061add90d8bd8517602ae62eccc01015e
2ujin <suddenlydisappear200@gmail.com>
2020-10-04 13:42:24 +0900

    Add .gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-// import express from 'express'
 import express from 'express'
 const app = express()
 const port = 4000
 
 app
     .get('/', (req, res) => res.json({ message: 'hello world' }))
-    .listen(port, ()=> console.log(`http://localhost:${port} started ... `))
+    .get('/hello/:skip/:limit', (req, res) => {
+        console.log(req.params)
+        res.json(req.params)
+    })
 
+    .listen(port, ()=> console.log(`http://localhost:${port} started ... `))
     
