07da78683d31283f1ac1ebd4f6523060118ee1c7
2ujin <suddenlydisappear200@gmail.com>
2020-10-04 13:34:20 +0900

    Refactor node_modules

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("hello world ~ ")
+// import express from 'express'
+import express from 'express'
+const app = express()
+const port = 4000
+
+app
+    .get('/', (req, res) => res.json({ message: 'hello world' }))
+    .listen(port, ()=> console.log(`http://localhost:${port} started ... `))
+
+    
